d4da3be4db5a405f1ee9caacfcd8699b2c163134
mkonemo <mkonemo@gmail.com>
2021-09-17 11:58:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-xmzqtb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-xmzqtb)
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+
+import ExpenseItem from './ExpenseItem';
+
+import './Expenses.css';
+
+const Expenses = props => {
+  const [filteredYear, setFilteredYear] = useState('2021');
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {props.items.map(expense => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
