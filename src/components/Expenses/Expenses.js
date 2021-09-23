b30fd3e8a444321dbc8d658efb8d8566e3fdeff6
mkonemo <mkonemo@gmail.com>
2021-09-23 16:25:22 +0200

    1 file modified

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,9 +13,21 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
-  })
+  });
+
+let expenseContent = <p>No expense found</p>
+if(filteredExpenses.length > 0) {
+  expenseContent = filteredExpenses.map((expense) => (
+    <ExpenseItem
+      key={expense.id}
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+    />
+  ))
+}
 
   return (
     <div>
@@ -24,14 +36,19 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+{expenseContent}
+        {/*filteredExpenses.length === 0 ? (
+          <p>No expenses found</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+          )*/}
       </Card>
     </div>
   );
