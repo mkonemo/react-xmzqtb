426f081bc075ece8f247dffce7108aed6a259f21
mkonemo <mkonemo@gmail.com>
2021-09-28 17:12:33 +0200

    1 file modified

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -14,7 +14,7 @@ const Chart = (props) => {
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalMaximum}
           label={dataPoint.label}
         />
       ))}
